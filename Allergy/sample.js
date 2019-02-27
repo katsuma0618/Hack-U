6e1bbcff9b878533f11e2fd9de3ce6bd25648356
katsuma0618 <katsuma0618@yahoo.co.jp>
2019-02-27 17:15:50 +0900

    base

---
+++ Allergy/sample.js
@@ -0,0 +1,24 @@
+JavaScript
+//マウスオーバー時の処理
+function mouseOn(){
+  var obj = document.getElementById("img1");
+  obj.src = "images/store1.png";
+}
+
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("img1");
+  obj.src = "images/b.png";
+}
+
+//マウスオーバー時の処理
+// function mouseOn(){
+//   var obj = document.getElementById("img1");
+//   obj.src = "cat2.png";
+// }
+//
+// //マウスアウト時の処理
+// function mouseOff(){
+//   var obj = document.getElementById("img1");
+//   obj.src = "cat1.png";
+// }
